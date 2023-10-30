c3e4efb811d8e44f97ae9f16196352db52e5ed00
Michail Mygas <143801687+mikemgs@users.noreply.github.com>
2023-10-30 12:45:22 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.scss';
+import React from 'react';
 
 function App() {
+  const [theme, setTheme] = React.useState('light')
+
+  const handler = (theme) => {
+    setTheme(theme)
+  }
+
   return (
-    <div className="App">
-      <button>test</button>
+    <div className={`App ${theme}`}>
+      <button onClick={() => handler(theme === 'light'? 'dark': 'light')}>theme - {theme}</button>
     </div>
   );
 }
